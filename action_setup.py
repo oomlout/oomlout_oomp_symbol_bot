8f09dd6d829313990a957e3ec5396cacd05cdc13
Oom Lout <aaron@oomlout.com>
2023-08-03 18:12:24 +0100

    removed arturo duplicates

---
+++ action_setup.py
@@ -0,0 +1,17 @@
+import oomlout_oomp_symbol_bot
+
+def main():
+    oomlout_oomp_symbol_bot.load_data()
+    oomlout_oomp_symbol_bot.copy_data()
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
