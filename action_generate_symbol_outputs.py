83f32372997866931bdb996c2bf1c19cc2e30db0
Oom Lout <aaron@oomlout.com>
2023-08-04 16:55:50 +0100

    comitting after 1 generations

--- action_generate_symbol_outputs.py
+++ action_generate_symbol_outputs.py
@@ -4,7 +4,7 @@ import os
 
 def go_through_directories():
     # go through all directories in footprints
-    count = 0
+    count = 1
     for root, dirs, files in os.walk("symbols"):
         #for each directory
         for name in dirs:
@@ -14,14 +14,20 @@ def go_through_directories():
             for file in os.listdir(os.path.join(root, name)):
                 #if kicad_mod file
                 if file.endswith(".kicad_sym"):
+                    #exclude filter array
+                    filter_exclude = "fpga"
+                    #if not in filter
                     filename = os.path.join(root, name, file)
+                    if filter_exclude not in filename.lower():
+                        
+                        
 
-                    counter = oom_kicad.generate_outputs_symbol(filename=filename)
+                        counter = oom_kicad.generate_outputs_symbol(filename=filename)
 
-                    count += counter
-                    #commit to github using oom_kicad after 250 files
-                    if count % 250 == 0:
-                        oom_kicad.push_to_git()
+                        count += counter
+                        #commit to github using oom_kicad after 250 files
+                        if count % 2 == 0:
+                            oom_kicad.push_to_git()
 
 
 
