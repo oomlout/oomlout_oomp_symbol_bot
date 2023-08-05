dc314a038a9e40f6dae36476b242fdeda1b4c65e
Oom Lout <aaron@oomlout.com>
2023-08-05 16:50:52 +0100

    some resolutions

---
+++ action_generate_image_resolutions.py
@@ -0,0 +1,36 @@
+import oom_base
+
+
+
+def main():
+    #go through all files in symbols/
+    import os
+    count = 1   
+    for root, dirs, files in os.walk("symbols"):
+        #for each directory
+        for name in dirs:
+            #go through the files in this directory just one level
+            for file in os.listdir(os.path.join(root, name)):
+                #if kicad_mod file
+                if file.endswith(".png") or file.endswith(".jpg") or file.endswith(".jpeg"):
+                    resolutions = [140,300,600,1000]
+                    for resolution in resolutions:
+                        #generate the image at this resolution
+                        filename = os.path.join(root, name, file)
+                        #print(filename)
+                        oom_base.generate_image(filename=filename, resolution=resolution)
+                    pass
+                    count += 1
+                    #print a dot every 1000 files
+                    if count % 100 == 0:
+                        print(".", end="", flush=True)
+                    
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
