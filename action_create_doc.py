db95366562b53b50d410da620c6b3b2bc64aca45
Oom Lout <aaron@oomlout.com>
2023-08-09 18:48:02 +0100

    added creating doc

--- action_create_doc.py
+++ action_create_doc.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import oom_base
 
 def main():
     directory_doc = "c:/gh/oomlout_oomp_symbol_doc"
@@ -10,19 +11,36 @@ def main():
     for root, dirs, files in os.walk("symbols"):
         #for every file
         for file in files:
-            full_file = os.path.join(root, file)
-            #replace \\ with /
-            full_file = full_file.replace("\\", "/")
-            src = full_file
-            file_slashes = full_file.replace("_", "/")
-            dst = os.path.join(directory_doc, file_slashes)
-            #replace \\ with /
-            dst = dst.replace("\\", "/")
-            #copy and overwrite if there using shutil
-            #if directories don't exist create them
-            if not os.path.exists(os.path.dirname(dst)):
-                os.makedirs(os.path.dirname(dst))
-            shutil.copyfile(src, dst)
+            if "working.yaml" in file:
+                directory = root.replace("\\", "/")
+
+                src = os.path.join(root, file)
+                src = src.replace("\\", "/")
+
+                yaml_file = f'{directory}/{file}'
+                yaml_file = yaml_file.replace("\\", "/")
+                #load yaml file
+                import yaml
+                with open(yaml_file) as f:
+                    yaml_dict = yaml.load(f, Loader=yaml.FullLoader)
+                owner = yaml_dict.get("owner", "")
+                library = yaml_dict.get("library_name", "")
+                library = library.lower()
+                #replace special characters using oom_base
+                library = oom_base.remove_special_characters(library)
+                
+                symbol_name = yaml_dict.get("name", "")
+                symbol_name = symbol_name.replace(f"{library}_", "")
+
+                dst = f"c:/gh/oomlout_oomp_symbol_doc/symbols/{owner}/{library}/{symbol_name}"
+
+                #copy all files in source directory inclusion subfodlers to dst directory using shutil
+                print(f"Copying {src} to {dst}")
+                shutil.copytree(directory, dst, dirs_exist_ok=True)
+
+
+                pass
+                
             
 
 
