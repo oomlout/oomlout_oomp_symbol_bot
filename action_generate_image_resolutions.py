b9d75fdc5e8eb68ab67bf93b39d90a0e7b875654
Oom Lout <aaron@oomlout.com>
2023-09-02 16:23:43 +0100

    comitting after 5000 generations

--- action_generate_image_resolutions.py
+++ action_generate_image_resolutions.py
@@ -6,6 +6,7 @@ def main():
     #go through all files in symbols/
     import os
     count = 1   
+    count2 = 1
     for root, dirs, files in os.walk("symbols"):
         #for each directory
         for name in dirs:
@@ -18,13 +19,16 @@ def main():
                         #generate the image at this resolution
                         filename = os.path.join(root, name, file)
                         #print(filename)
-                        oom_base.generate_image(filename=filename, resolution=resolution)
+                        counter = oom_base.generate_image(filename=filename, resolution=resolution)
                         pass
-                        count += 1
+                        count += counter
+                        count2 += 1
                         #print a dot every 1000 files
+                        if count2 % 1000 == 0:
+                            print(".", end="", flush=True)
                         if count % 100 == 0:
                             print(".", end="", flush=True)
-                        if count % 10000 == 0:
+                        if count % 5000 == 0:
                             oom_kicad.push_to_git(count=count)
     oom_kicad.push_to_git(count=count)
 
